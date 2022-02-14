b93b2234da124b557dd80e45e3c0a5782fdcfe2a
vinayski <ski@sankhe.com>
2022-02-13 18:24:15 -0800

    add protection of the branch

--- index.js
+++ index.js
@@ -3,17 +3,36 @@
  * @param {import('probot').Probot} app
  */
 module.exports = (app) => {
-  app.log.info("protector is enabled!");
-  app.on("repository.created",async (context) => {
-        app.log(context);
-        app.log(context.repo.name);
-        const repo = context.repo();
-        const issueParams = {
-              title: "My First Issue",
-              body: "Testing Issue Creation"
+    app.log.info("protector is enabled!");
+    app.on("repository.created", async (context) => {
+            const repo = context.payload.repository.name
+
+            const owner = context.payload.repository.owner.login;
+            const issueParams = {
+                owner: owner,
+                repo: repo,
+                title: "Protection on Main Branch enabled",
+                body: "@vinayski "
+            }
+            const protectionParams = {
+                owner: owner,
+                repo: repo,
+                branch: 'main',
+                enforce_admins: null,
+                required_pull_request_reviews: {
+                    bypass_pull_request_allowances: {},
+                    dismiss_stale_reviews: true,
+                    dismissal_restrictions: {},
+                    require_code_owner_reviews: true,
+                    required_approving_review_count: 2
+                },
+                required_status_checks: null,
+                restrictions: null,
+            }
+            app.log(protectionParams)
+            app.log(issueParams)
+            await context.octokit.repos.updateBranchProtection(protectionParams);
+            await context.octokit.issues.create(issueParams);
         }
-        const params = Object.assign({}, repo,  issueParams)
-        await context.github.issues.create(params);
-      }
-  );
+    );
 };

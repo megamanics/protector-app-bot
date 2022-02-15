8e31944eba47b9cdabe90b0182c65159bae456af
vinayski <ski@sankhe.com>
2022-02-15 14:25:53 -0800

    remove unused constant

--- index.js
+++ index.js
@@ -28,21 +28,8 @@ module.exports = (app) => {
                 required_status_checks: null,
                 restrictions: null,
             };
-            const required_pull_request_reviews_params = {
-                owner: owner,
-                repo: repo,
-                branch: 'main',
-                required_pull_request_reviews: {
-                    bypass_pull_request_allowances: {},
-                    dismiss_stale_reviews: true,
-                    dismissal_restrictions: {},
-                    require_code_owner_reviews: true,
-                    required_approving_review_count: 2
-                }
-            };
             app.log(protectionParams);
             app.log(issueParams);
-            //app.log(context.octokit.repos.updateBranchProtection.required_pull_request_review);
             await context.octokit.repos.updateBranchProtection(protectionParams);
             await context.octokit.issues.create(issueParams);
         }

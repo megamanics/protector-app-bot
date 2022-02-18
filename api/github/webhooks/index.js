e530072823b38132da0372921592296d80297157
vinayski <ski@sankhe.com>
2022-02-17 23:15:39 -0700

    Update index.js

--- api/github/webhooks/index.js
+++ api/github/webhooks/index.js
@@ -1,9 +1,4 @@
 const { createNodeMiddleware, Probot } = require("probot");
 const app = require("../../../index.js");
-const probot = new Probot({
-  appId: 172112,
-  privateKey: process.env.PK,
-  secret: process.env.SECRET
-});
-
+const probot = new Probot();
 module.exports = createNodeMiddleware(app, { probot, webhooksPath: '/api/github/webhooks'});

998d4289c590f0e27cd4b2b031a0c4a5290b722f
vinayski <ski@sankhe.com>
2022-02-18 22:22:47 -0700

    console log

--- api/github/webhooks/index.js
+++ api/github/webhooks/index.js
@@ -1,3 +1,4 @@
+console.log("index.js loaded")
 const { createNodeMiddleware, createProbot } = require("probot");
 const app = require("../../../app");
 const probot = createProbot({
@@ -5,5 +6,4 @@ const probot = createProbot({
   privateKey: process.env.PRIVATE_KEY,
   secret: process.env.WEBHOOK_SECRET
 });
-
 module.exports = createNodeMiddleware(app, { probot, webhooksPath: '/api/github/webhooks' });
